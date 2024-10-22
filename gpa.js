f49ab9fe23f551efe4ef5dac6d944f02a56fa0bb
halazYoha <hailemariamyohannes07@gmail.com>
2024-10-22 21:39:13 +0300

    Add files via upload

---
+++ gpa.js
@@ -0,0 +1,363 @@
+
+var a1=0
+var a2=0
+var a3=0
+var a4=0
+var a5=0
+var a6=0
+var a7=0
+function myfun(){
+  var  x=document.getElementById('inc').value
+ var y
+if(x>=85&&  x<=100){
+    a1=4
+    y=a1
+
+}
+else if(x>=80&&x<85){
+    a1=3.75
+    y=a1
+
+}
+ else if(x>=75&&x<80){
+    a1=3.5
+    y=a1
+
+}
+ else if(x>=70&&x<75){
+    a1=3
+    y=a1
+
+}
+ else if(x>=65&&x<70){
+    a1=2.75
+    y=a1
+
+}
+ else if(x>=60&&x<65){
+    a1=2.5
+    y=a1
+
+}
+else if(x>=55&&x<60){
+    a1=2
+    y=a1
+
+}
+else if(x>=50&&x<55){
+    a1=1.75
+    y=a1
+
+}
+else if(x>=45&&x<50){
+    a1=1.5
+    y=a1
+
+}
+else if(x<45){
+    a1=1.00
+    y=a1
+
+}
+
+  var  m=document.getElementById('eco').value
+ var z
+if(m>=85&&  m<=100){
+    a2=4
+    z=a2
+}
+else if(m>=80&&m<85){
+    a2=3.75
+    z=a2
+}
+ else if(m>=75&&m<80){
+    a2=3.5
+    z=a2
+}
+ else if(m>=70&&m<75){
+    a2=3
+    z=a2
+}
+ else if(m>=65&&m<70){
+    a2=2.75
+    z=a2
+}
+ else if(m>=60&&m<65){
+    a2=2.5
+    z=a2
+}
+else if(m>=55&&m<60){
+    a2=2
+    z=a2
+}
+else if(m>=50&&m<55){
+    a2=1.75
+    z=a2
+
+}
+else if(m>=45&&m<50){
+    a2=1.5
+    z=a2
+
+}
+else if(m<45){
+    a2=1.00
+    z=a2
+
+}
+var  n=document.getElementById('fan').value
+var w
+if(n>=85&&  n<=100){
+   a3=4
+   w=a3
+}
+else if(n>=80&&n<85){
+   a3=3.75
+   w=a3
+}
+else if(n>=75&&n<80){
+   a3=3.5
+   
+   w=a3
+}
+else if(n>=70&&n<75){
+   a3=3
+   w=a3
+}
+else if(n>=65&&n<70){
+   a3=2.75
+   w=a3
+}
+else if(n>=60&&n<65){
+   a3=2.5
+   w=a3
+}
+else if(n>=55&&n<60){
+   a3=2
+   w=a3
+}
+else if(n>=50&&n<55){
+   a3=1.75
+   w=a3
+
+}
+else if(n>=45&&n<50){
+    a3=1.5
+    w=a3
+ 
+ }
+ else if(n<45){
+    a3=1.00
+    w=a3
+ 
+ }
+var o=document.getElementById('dis').value
+var r 
+if(o>=85&&  o<=100){
+   a4=4
+   r=a4
+}
+else if(o>=80&&o<85){
+   a4=3.75
+   r=a4
+}
+else if(o>=75&&o<80){
+   a4=3.5
+   r=a4
+}
+else if(o>=70&&o<75){
+   a4=3
+   r=a4
+}
+else if(o>=65&&o<70){
+   a4=2.75
+   r=a4
+}
+else if(o>=60&&o<65){
+   a4=2.5
+   r=a4
+}
+else if(o>=55&&o<60){
+   a4=2
+   r=a4
+}
+else if(o>=50&&o<55){
+   a4=1.75
+   r=a4
+
+}
+else if(o>=45&&o<50){
+    a4=1.5
+    r=a4
+ 
+ }
+ else if(o<45){
+    a4=1.00
+    r=a4
+ 
+ }
+var p=document.getElementById('pro').value
+var u 
+if(p>=85&&  p<=100){
+   a5=4
+   u=a5
+}
+else if(p>=80&&p<85){
+   a5=3.75
+   u=a5
+}
+else if(p>=75&&p<80){
+   a5=3.5
+   u=a5
+}
+else if(p>=70&&p<75){
+   a4=3
+   u=a5
+}
+else if(p>=65&&p<70){
+   a5=2.75
+   u=a5
+}
+else if(p>=60&&p<65){
+   a5=2.5
+   u=a5
+}
+else if(p>=55&&p<60){
+   a5=2
+   u=a5
+}
+else if(p>=50&&p<55){
+   a5=1.75
+   u=a5
+
+}
+else if(p>=45&&p<50){
+    a5=1.5
+    u=a5
+ 
+ }
+ else if(p<45){
+    a5=1.00
+    u=a5
+ 
+ }
+var q=document.getElementById('int').value
+var t 
+if(q>=85&&  q<=100){
+   a6=4
+   t=a6
+}
+else if(q>=80&&q<85){
+   a6=3.75
+   t=a6
+}
+else if(q>=75&&q<80){
+   a6=3.5
+   t=a6
+}
+else if(q>=70&&q<75){
+   a6=3
+   t=a6
+}
+else if(q>=65&&q<70){
+   a6=2.75
+   t=a6
+}
+else if(q>=60&&q<65){
+   a6=2.5
+   t=a6
+}
+else if(q>=55&&q<60){
+   a6=2
+   t=a6
+}
+else if(q>=50&&q<55){
+   a6=1.75
+   t=a6
+
+}
+else if(q>=45&&q<50){
+    a6=1.5
+    t=a6
+ 
+ }
+ else if(q<45){
+    a6=1.00
+    t=a6
+ 
+ }
+var s=document.getElementById('sta').value
+var v 
+if(s>=85&&  s<=100){
+   a7=4
+   v=a7
+}
+else if(s>=80&&s<85){
+   a7=3.75
+   v=a7
+}
+else if(s>=75&&s<80){
+   a7=3.5
+   v=a7
+}
+else if(s>=70&&s<75){
+   a7=3
+   v=a7
+}
+else if(s>=65&&s<70){
+   a7=2.75
+   v=a7
+}
+else if(s>=60&&s<65){
+   a7=2.5
+   v=a7
+}
+else if(s>=55&&s<60){
+   a7=2
+   v=a7
+}
+else if(s>=50 && s<55){
+   a7=1.75
+   v=a7
+
+}
+else if(s>=45&&s<50){
+    a7=1.5
+    v=a7
+ 
+ }
+ else if(s<45){
+    a7=1.00
+    v=a7
+ 
+ }
+ var a=document.getElementById('sub1').value
+ var b=document.getElementById('sub2').value
+ var c=document.getElementById('sub3').value
+ var d=document.getElementById('sub4').value
+ var e=document.getElementById('sub5').value
+ var f=document.getElementById('sub6').value
+ var g=document.getElementById('sub7').value
+var sum = a*y + b*z+ c*w + d*u + e*r + f*t + g*v
+ var tot = a*1 + b*1 + c*1 + d*1 + e*1 + f*1 + g*1
+gpa = sum /tot
+document.getElementById('par').innerHTML= "your CGPA = " + gpa
+if(gpa>=1.75 && gpa<=4){
+    document.getElementById('par1').innerHTML="PROMOTED!!"
+}
+else if(gpa >=1.5 && gpa < 1.75 ){
+    document.getElementById('par1').innerHTML="WARNING"
+}
+else if (gpa >= 1 && gpa<1.5)
+    document.getElementById('par1').innerHTML="ACADAMIC DISMISSAL"
+else if(gpa <1){
+    document.getElementById('par1').innerHTML="COMPLATE DISMISSAL"
+}
+}
+
+
+
+
+
+
+
